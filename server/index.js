89879e662a5794ca2af73421b22ffd8325fe93b4
Cody Tomesch <tomeschcody@gmail.com>
2018-06-19 11:34:50 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const app = require('express')()
+    , bodyParser = require('body-parser')
+    , nodemailer = require('nodemailer');
+
+    const { EMAIL, EMAIL_PASSWORD } = process.env
+
+    const transport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: EMAIL,
+            pass: EMAIL_PASSWORD
+        }
+    })
+
+
+    app.use(bodyParser.json());
+
+    app.post('/api/email', (req, res) => {
+        const {name, email, message} = req.body
+        const mailOptions = {
+            from: email,
+            to: 'tomeschcody@gmail.com',
+            subject: 'An inquiry from ' + name,
+            text: `Email: ${email}
+${message}`
+        }
+
+        transport.sendMail(mailOptions, (error, response) => {
+            if(error) {
+                console.log(error)
+            } else {
+                res.sendStatus(200)
+            }
+        })
+    })
+
+    app.listen(3005, () => console.log('Serve us up some ports', 3005))
